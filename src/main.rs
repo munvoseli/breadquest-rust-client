c38f0d06290e028e30c9d22c7892aaa2506d7dde
munvoseli <muslinmelody@gmail.com>
2021-12-17 22:36:09 -0500

    allow multiple accounts

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,34 @@
 // you know what a good name for this is?
 // crust
 // breadquest and rust
-use websocket::header::{Headers, Cookie};
+use std::thread;
 use std::fs::File;
+use std::future::Future;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
 use std::convert::TryInto;
 //use std::convert::TryFrom;
-use std::sync::Arc;
 use std::io::{Write, Read, stdout};
-use websocket::ws::Message;
 //use std::io;
 
 mod qc;
-
-struct Player {
-	x: i32,
-	y: i32,
-}
+mod signin;
 
 struct Enemy { x: i32, y: i32, }
+
+struct Player {
+	pindex: i32,
+	x: i32,
+	y: i32,
+	user: String,
+	comque: Vec<String>,
+	enemies: Vec<Enemy>,
+	tile_arr: [u8; 128*128],
+	tcp: websocket::client::sync::Client<native_tls::TlsStream<std::net::TcpStream>>
+}
+
 
 /* i am not entirely convinced
    that the song sunny came home
@@ -30,14 +37,11 @@ struct Enemy { x: i32, y: i32, }
    especially that list of names stanza
    */
 fn main() {
-	println!("Hello, world!");
-	let infostr = get_login_name();
-	println!("Login info: {} {}", infostr.0, infostr.1);
-	let tcpclient = login_socket(infostr.0, infostr.1);
-	lop(tcpclient);
+	println!("Hello, world!"); // this is literally my hello cargo project and i am not removing this line
+	lop();
 }
 
-fn get_login_name() -> (String, String) {
+fn get_login_name() -> Vec<String> {
 	let path = std::path::Path::new("config.txt");
 	let mut file = match File::open(&path) {
 		Err(e) => panic!("Couldn't open config file: {}", e),
@@ -49,96 +53,37 @@ fn get_login_name() -> (String, String) {
 		Ok(file) => file
 	};
 	let infvec: Vec<&str> = s.split("\n").collect();
-	(String::from(infvec[0]), String::from(infvec[1]))
+	//(String::from(infvec[0]), String::from(infvec[1]))
+	infvec.iter().map(|&x| String::from(x)).collect()
 }
 
-fn login_socket(user: String, pass: String) -> websocket::client::sync::Client<native_tls::TlsStream<std::net::TcpStream>> {
-	let consid = login(user, pass);
-	let tcpclient = sus_socket(consid);
-	tcpclient.set_nonblocking(true).unwrap();
-	tcpclient
-}
-
-
-fn sus_socket(consid: String) -> websocket::client::sync::Client<native_tls::TlsStream<std::net::TcpStream>> {
-	let connector = native_tls::TlsConnector::new().unwrap();
-	let mut headers_owo = Headers::new();
-	headers_owo.set(Cookie(vec![consid]));
-	let mut client = websocket::ClientBuilder::new("wss://ostracodapps.com:2626/gameUpdate")
-		.unwrap().custom_headers(&headers_owo)
-		.connect_secure(Some(connector)).unwrap();
-	let message = websocket::Message::text("[{\"commandName\": \"startPlaying\"},{\"commandName\": \"getTiles\", \"size\": 31},{\"commandName\":\"assertPos\", \"pos\":{\"x\":0,\"y\":0}}, {\"commandName\": \"getEntities\"}]");
-	client.send_message(&message).unwrap();
-	//println!("{}", client.buffered_read_size().unwrap());
-//	let resp = client.recv_message().unwrap();
-//	println!("{:?}", resp);
-	client
-
-}
-
-fn login(user: String, pass: String) -> String {
-	let mut root_store = rustls::RootCertStore::empty();
-	root_store.add_server_trust_anchors(
-		webpki_roots::TLS_SERVER_ROOTS
-		.0
-		.iter()
-		.map(|ta| {
-			rustls::OwnedTrustAnchor::from_subject_spki_name_constraints(
-				ta.subject, ta.spki, ta.name_constraints
-			)
-		})
-	);
-	let config = rustls::ClientConfig::builder()
-		.with_safe_defaults()
-		.with_root_certificates(root_store)
-		.with_no_client_auth();
-	let example_com = "ostracodapps.com".try_into().unwrap();
-	let mut clientconn = rustls::ClientConnection::new
-		(Arc::new(config), example_com).unwrap();
-	//clientconn.writer().write(b"GET / HTTP/1.1\r\n\r\n").unwrap();
-	let mut socket = std::net::TcpStream::connect("ostracodapps.com:2626").unwrap();
-	let mut tls = rustls::Stream::new(&mut clientconn, &mut socket);
-
-	let clen = user.len() + pass.len() + 19;
-	let fstr = format!("POST /loginAction HTTP/1.1\r\nHost: ostracodapps.com\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: {}\r\n\r\nusername={}&password={}\r\n", clen, user, pass);
-	println!("{}", clen);
-	tls.write(fstr.as_bytes()).unwrap();
-	let ciphersuite = tls.conn.negotiated_cipher_suite().unwrap();
-	println!("Current ciphersuite: {:?}", ciphersuite.suite());
-	let mut pt = Vec::new();
-	tls.read_to_end(&mut pt).unwrap();
-	stdout().write_all(&pt).unwrap();
-	println!();
-
-	let strig = String::from_utf8(pt).unwrap();
-	let considn = strig.find("set-cookie").unwrap() + 12;
-	let considm = &strig[considn..].find(";").unwrap();
-	(&strig[considn..considn+considm]).to_string()
-}
-
-fn lop(mut tcpclient: websocket::client::sync::Client<native_tls::TlsStream<std::net::TcpStream>>) {
+fn lop() {
+//	let infostr = get_login_name();
+//	println!("Login info: {} {}", infostr.0, infostr.1);
+//	let tcpclient = login_socket(infostr.0, infostr.1);
 	let sdl_context = sdl2::init().unwrap();
 	let video_subsystem = sdl_context.video().unwrap();
-	let window = video_subsystem.window("h", 600, 600)
+	let window = video_subsystem.window("h", 1800, 600)
 	.position_centered().build().unwrap();
 	let mut canvas = window.into_canvas().build().unwrap();
 	let mut i:i32 = 0;
 	let mut event_pump = sdl_context.event_pump().unwrap();
-	let mut tile_arr: [u8; 128*128] = [60; 128*128];
-	let mut player: Player = Player {x: 0, y: 0};
-	let mut enemies: Vec<Enemy> = Vec::new();
-	let mut command_vec: Vec<String> = Vec::new();
+	let mut players: Vec<Player> = Vec::new();
+	let infvec = get_login_name();
+	for i in 0..(infvec.len() / 2) {
+		let user = &infvec[i * 2];
+		let pass = &infvec[i * 2 + 1];
+		let tcpclient = signin::login_socket(user.to_string(), pass.to_string());
+		let mut player = Player {pindex: i as i32, x: 0, y: 0, user: user.to_string(), tile_arr: [60; 128*128], enemies: Vec::new(), comque: Vec::new(), tcp: tcpclient};
+		qc::initial_commands(&mut player.comque);
+		println!("Set up player {}", player.user);
+		players.push(player);
+	}
+//	let mut future_players: Vec<Box<dyn Future<Output=websocket::client::sync::Client<native_tls::TlsStream<std::net::TcpStream>>>>> = Vec::new();
+//	let infostr = get_login_name();
+//	future_players.push(Box::new(login_socket(infostr.0, infostr.1)));
 	'running: loop {
-		if i % 32 == 0 {
-			qc::get_entities(&mut command_vec);
-		}
-//		let j:u8 = (i % 256) as u8;
-//		canvas.set_draw_color(Color::RGB(0,0,0));
-//		canvas.clear();
-//		canvas.set_draw_color(Color::RGB(j, 64, 255 - j));
-//		let r = sdl2::rect::Rect::new(i%256,(i/2)%500, 100,100);
-//		canvas.fill_rect(Some(r)).ok();
-		draw(&mut canvas, tile_arr, &player, &enemies);
+		canvas.present();
 		for event in event_pump.poll_iter() {
 			match event {
 				Event::Quit {..} |
@@ -148,53 +93,56 @@ fn lop(mut tcpclient: websocket::client::sync::Client<native_tls::TlsStream<std:
 				_ => {}
 			}
 		}
-		canvas.present();
-//		::std::thread::sleep(Duration::new(0, 1000000000u32 / 60));
-//		::std::thread::sleep(Duration::new(0, 16666000u32));
-		::std::thread::sleep(Duration::new(0, 1000000000u32 / 32));
-		'message_loop: for message in tcpclient.incoming_messages() {
+
+/*		// check on the futures
+		for boxfuttls in future_players {
+			match std::pin::Pin::new(&mut *boxfuttls).poll() {
+				std::task::Poll::Pending => {},
+				std::task::Poll::Ready(val) => {
+					println!("{}", val);
+				}
+			}
+		}
+*/
+		// now do things for each player
+		for mut player in &mut players {
+		draw(&mut canvas, &player);
+		if i % 64 == 0 {
+			qc::get_entities(&mut player.comque);
+		}
+		'message_loop: for message in player.tcp.incoming_messages() {
 			let message = match message {
 				Ok(m) => m,
-				Err(e) => {break 'message_loop;}
+				Err(_) => {break 'message_loop;}
 			};
 			let vecstr = match message {
 				websocket::OwnedMessage::Text(stri) => stri,
 				_ => {break 'message_loop;}
 			};
-//			println!("{:?}", message);
-//			let mut strr: Vec<u8> = Vec::new();
-//			message.unwrap().serialize(&mut strr, false).unwrap();
-//			println!("{:?}", strr);
-//			for i in 0..message.unwrap().payload.len() {
-//				strr.push(message.unwrap().payload[i]);
-//			}
-//			let vecstr = String::from_utf8((&strr[0..]).to_vec()).unwrap();
-//			let vecstr = String::from(message.unwrap());
-			println!("{:?}", vecstr);
+//			println!("{:?}", vecstr);
 			let respdata = json::parse(&vecstr).unwrap();
-/*			println!("{:?}", respdata);
-			println!("{:?}", respdata.dump());
-			println!("{:?}", respdata["commandList"]);
-*/			for command in respdata["commandList"].members() {
+			for command in respdata["commandList"].members() {
 				let typ:&str = command["commandName"].as_str().unwrap();
-				println!("{:?}", command.dump());
+				println!("{} {:?}\n", player.user, command.dump());
 				let ty = String::from(typ);
 				if ty.eq("setTiles") {
-					cq_set_tiles(command, &mut tile_arr);
+					cq_set_tiles(command, &mut player.tile_arr);
 				} else if ty.eq("setLocalPlayerPos") {
 					player.x = command["pos"]["x"].as_i32().unwrap();
 					player.y = command["pos"]["y"].as_i32().unwrap();
 				} else if ty.eq("addEntity") {
-					cq_add_entity(command, &mut enemies);
+					cq_add_entity(command, &mut player.enemies);
 				} else if ty.eq("removeAllEntities") {
-					enemies = Vec::new();
+					player.enemies = Vec::new();
 				}
 			}
 		}
+		qc::send_commands(&mut player.tcp, &player.comque);
+		player.comque = Vec::new();
+		}
 //		println!("{:?}", tcpclient.into_stream());
 		i = i + 1;
-		qc::send_commands(&mut tcpclient, &command_vec);
-		command_vec = Vec::new();
+		::std::thread::sleep(Duration::new(0, 1000000000u32 / 32));
 	}
 }
 
@@ -246,7 +194,7 @@ fn get_tile_at_relpos(player: &Player, xrel: i32, yrel: i32, tile_arr: [u8; 128*
 	return tile_arr[i];
 }
 
-fn draw(canvas: &mut sdl2::render::Canvas<sdl2::video::Window>, tile_arr: [u8; 128*128], player: &Player, enemies: &Vec<Enemy>) {
+fn draw(canvas: &mut sdl2::render::Canvas<sdl2::video::Window>, player: &Player) {
 //	let mut i = 0;
 //	for x in 0..128 {
 //		for y in 0..128 {
@@ -256,10 +204,10 @@ fn draw(canvas: &mut sdl2::render::Canvas<sdl2::video::Window>, tile_arr: [u8; 1
 //			i = i + 1;
 //		}
 //	}
-	let shsize: i32 = 30;
+	let shsize: i32 = 25;
 	for y in -shsize..shsize {
 		for x in -shsize..shsize {
-			let tile = get_tile_at_pos(player.x + x, player.y + y, tile_arr);
+			let tile = get_tile_at_pos(player.x + x, player.y + y, player.tile_arr);
 			if tile >= 0x80 && tile <= 0x89 {
 				canvas.set_draw_color(Color::RGB(
 							[255,255,255,255,  0,  0,  0,255,170][tile as usize - 0x80],
@@ -268,16 +216,16 @@ fn draw(canvas: &mut sdl2::render::Canvas<sdl2::video::Window>, tile_arr: [u8; 1
 			} else {
 				canvas.set_draw_color(Color::RGB(tile, tile, tile));
 			}
-			let r = sdl2::rect::Rect::new((x + shsize) * 8, (y + shsize) * 8, 8, 8);
+			let r = sdl2::rect::Rect::new((x + shsize + 60 * player.pindex) * 8, (y + shsize) * 8, 8, 8);
 			canvas.fill_rect(Some(r)).ok();
 		}
 	}
-	canvas.set_draw_color(Color::RGB(0,0,0));
-	for enemy in enemies {
+	canvas.set_draw_color(Color::RGB(255,0,85));
+	for enemy in &player.enemies {
 		let relx = enemy.x - player.x;
 		let rely = enemy.y - player.y;
 		if relx.abs() < shsize && rely.abs() < shsize {
-			let r = sdl2::rect::Rect::new((relx + shsize) * 8 + 1, (rely + shsize) * 8 + 1, 6, 6);
+			let r = sdl2::rect::Rect::new((relx + shsize + 60 * player.pindex) * 8 + 1, (rely + shsize) * 8 + 1, 6, 6);
 			canvas.fill_rect(Some(r)).ok();
 		}
 	}
--- src/qc.rs
+++ src/qc.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub fn send_commands(
 	tcpclient: &mut websocket::client::sync::Client<native_tls::TlsStream<std::net::TcpStream>>,
 	command_vec: &Vec<String>
@@ -18,3 +20,24 @@ pub fn get_entities(command_vec: &mut Vec<String>) {
 	command_vec.push("{\"commandName\": \"getEntities\"}".to_string());
 }
 
+pub fn start_playing(command_vec: &mut Vec<String>) {
+	command_vec.push("{\"commandName\": \"startPlaying\"}".to_string());
+}
+
+pub fn assert_pos(command_vec: &mut Vec<String>) {
+	command_vec.push("{\"commandName\":\"assertPos\", \"pos\":{\"x\":0,\"y\":0}}".to_string());
+}
+
+pub fn get_tiles(command_vec: &mut Vec<String>) {
+	command_vec.push("{\"commandName\":\"getTiles\",\"size\":31}".to_string());
+}
+
+pub fn remove_tile(command_vec: &mut Vec<String>, dir: u8) {
+	command_vec.push(format!("{{\"commandName\":\"removeTile\",\"direction\":{}}}", dir));
+}
+
+pub fn initial_commands(command_vec: &mut Vec<String>) {
+	start_playing(command_vec);
+	assert_pos(command_vec);
+	get_tiles(command_vec);
+}
